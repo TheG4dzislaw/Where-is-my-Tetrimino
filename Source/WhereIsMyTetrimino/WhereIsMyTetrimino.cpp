bc725827917bc13039d36b573b67fcffa12a9b6c
RlyehStation\TyGadzieTy <g4dzilla@protonmail.com>
2023-12-20 22:34:55 +0100

    Empty repository was created

---
+++ Source/WhereIsMyTetrimino/WhereIsMyTetrimino.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WhereIsMyTetrimino.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WhereIsMyTetrimino, "WhereIsMyTetrimino" );
---
+++ Source/WhereIsMyTetrimino/WhereIsMyTetriminoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WhereIsMyTetriminoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEREISMYTETRIMINO_API AWhereIsMyTetriminoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
